9d74072fe37d37e98695122e0ab015bc39a570f9
kzhang <29695346+kzhang14@users.noreply.github.com>
2020-01-05 16:04:48 -0600

    Add files via upload

---
+++ Utilities/pid2docker.sh
@@ -0,0 +1,6 @@
+#~/bin/bash -e
+# Prints the name of the container inside which the process with a PID on the host is.
+
+
+  #pid="$1"
+  docker inspect --format '{{.Name}}' "$(cat /proc/1/cgroup |head -n 1 |cut -d / -f 3)" | sed 's/^\///'
